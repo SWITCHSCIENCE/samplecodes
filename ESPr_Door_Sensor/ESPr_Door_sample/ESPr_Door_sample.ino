48be2ead9e445fab9a5eedd0d251768fe4c76f1b
Soumura-Kazunori <soumura@switch-science.com>
2016-06-13 18:28:53 +0900

    ESPr_Door_Sensorサンプルスケッチ追加

---
+++ ESPr_Door_Sensor/ESPr_Door_sample/ESPr_Door_sample.ino
@@ -0,0 +1,104 @@
+
+#include <ESP8266WiFi.h>
+extern "C" {
+  #include "user_interface.h"
+}
+
+const char* ssid     = "ssid";
+const char* password = "password";
+
+const char* host = "host IP address";
+
+WiFiClient client;
+
+boolean flag = false;
+int LED = 4;
+int reed_sw = 5;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,HIGH);
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(reed_sw,INPUT);
+  digitalWrite(LED,LOW);
+}
+
+int value = 0;
+
+void loop() {
+  int door_state;
+  door_state = digitalRead(reed_sw);
+
+  Serial.print("Door State:");
+  if(door_state == 0){
+    Serial.println("Closed");
+  }
+  else{
+    Serial.println("Open");
+  }
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/cgi-bin/door.py/?";
+  if(door_state == 0){
+    url += "state=close";
+  }
+  else{
+    url += "state=open";
+  }
+    url += "&bat=";
+    url += system_adc_read();
+
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+  
+  Serial.println();
+  Serial.println("closing connection");
+
+  if(door_state == 0){
+    ESP.deepSleep(5*60*1000*1000, WAKE_RF_DEFAULT);
+  }
+  else{
+    ESP.deepSleep(60*60*1000*1000, WAKE_RF_DEFAULT);
+  }
+  delay(1000);
+  
+}
+
