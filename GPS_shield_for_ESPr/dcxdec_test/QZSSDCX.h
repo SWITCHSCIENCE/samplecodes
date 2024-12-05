7648cdf3098388eae7f0d505bd741ac7444b388f
arms22 <sasaki.yusuke@144lab.com>
2024-12-05 15:22:21 +0900

    3480 ESPr® Developer用GPSシールド SAM-M8Q搭載 DCReport/DCXデコードサンプルコード追加

---
+++ GPS_shield_for_ESPr/dcxdec_test/QZSSDCX.h
@@ -0,0 +1,101 @@
+#ifndef __QZSSDCX_H__
+#define __QZSSDCX_H__
+
+#include <Arduino.h>
+#include <stdint.h>
+
+#define DCX_MSG_UNKOWN 0
+#define DCX_MSG_L_ALERT 1
+#define DCX_MSG_J_ALERT 2
+#define DCX_MSG_MT_INFO 3
+#define DCX_MSG_OUTSIDE_JAPAN 4
+#define DCX_MSG_NULL 5
+
+struct DCXMessage {
+  uint32_t dcx_msg_type : 3;
+  uint32_t use_a12_to_a16 : 1;
+  uint32_t use_ex1 : 1;
+  uint32_t use_ex2_to_ex7 : 1;
+
+  // Satellite Designation (SD)
+  uint32_t sdmt : 1;
+  uint32_t sdm : 9;
+
+  // Common Alert Message Format (CAMF)
+  uint32_t a1_message_type : 2;
+  uint32_t a2_country_region_name : 9;
+  uint32_t a3_provider_identifier : 5;
+  uint32_t a4_hazard_category_type : 7;
+  uint32_t a5_severity : 2;
+  uint32_t a6_hazard_onset_week_number : 1;
+  uint32_t a7_hazard_onset_ToW : 14;
+  uint32_t a8_hazard_duration : 2;
+  uint32_t a9_selection_of_Library : 1;
+  uint32_t a10_version_of_library : 3;
+  uint32_t a11_guidance_to_react_library : 10;
+  uint32_t a12_ellipse_centre_latitude : 16;
+  uint32_t a13_ellipse_centre_longitude : 17;
+  uint32_t a14_ellipse_semi_major_axis : 5;
+  uint32_t a15_ellipse_semi_minor_axis : 5;
+  uint32_t a16_ellipse_azimuth_angle : 6;
+  uint32_t a17_main_subject_for_specific_settings : 2;
+  uint32_t a18_specific_settings : 15;
+
+  // L-Alert or Municipality-transmitted information
+  uint32_t ex1_target_area_code : 16;
+  uint32_t ex2_evacuate_direction_type : 1;
+  uint32_t ex3_additional_ellipse_centre_latitude : 17;
+  uint32_t ex4_additional_ellipse_centre_longitude : 17;
+  uint32_t ex5_additional_ellipse_semi_major_axis : 5;
+  uint32_t ex6_additional_ellipse_semi_minor_axis : 5;
+  uint32_t ex7_additional_ellipse_azimuth : 7;
+
+  // J-Alert
+  uint32_t ex8_target_area_code_list_type : 1;
+  uint32_t ex9_target_area_code_list[2];
+  uint32_t ex10_reserved : 3;
+
+  uint32_t vn_version_number : 6;
+};
+
+class DCXDecoder {
+public:
+  DCXMessage r;
+
+  DCXDecoder(){};
+  ~DCXDecoder(){};
+
+  bool decode(const uint8_t* l1smsg);
+  void printAll(Stream& stream, DCXMessage& r);
+  void printSummary(Stream& stream, DCXMessage& r);
+
+  static uint32_t get_bits(const uint8_t* l1smsg, int start_bits, int length);
+  static void binary_print(Stream& stream, uint32_t dat, int length);
+  static float calc_latitude_deg(uint32_t latitude);
+  static float calc_longitude_deg(uint32_t longitude);
+  static float calc_radius(uint32_t a);
+  static float calc_azimuth_angle(uint32_t a);
+  static float calc_additional_latitude_deg(uint32_t latitude);
+  static float calc_additional_longitude_deg(uint32_t longitude);
+  static float calc_additional_azimuth_angle(uint32_t a);
+
+  static const char* get_message_type_str(int message_type);
+  static const char* get_message_type_str_ja(int message_type);
+  static const char* get_country_region_name_str(int country_region_name);
+  static const char* get_country_region_name_str_ja(int country_region_name);
+  static const char* get_provider_name_japan(int provider_id);
+  static const char* get_hazard_category_and_type(int hazard_code);
+  static const char* get_hazard_category_and_type_ja(int hazard_code);
+  static const char* get_severity_str(int severity);
+  static const char* get_severity_str_ja(int severity);
+  static const char* get_hazard_onset_week_number_str_ja(int week_number);
+  static const char* get_hazard_onset_tow_str(int tow);
+  static const char* get_hazard_onset_tow_str_ja(int tow);
+  static const char* get_hazard_duration_str_ja(int duration);
+  static const char* get_municipality_code_japan(int code);
+  static const char* get_prefecture_name_ja(uint64_t bit_position);
+  static const char* get_guidance_instruction_library_ja(int code);
+  static const char* get_evacuate_direction_type_ja(int dir);
+};
+
+#endif
---
+++ GPS_shield_for_ESPr/espr_dev_qzss_drc_drx_decode/espr_dev_qzss_drc_drx_decode.ino
@@ -0,0 +1,282 @@
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+#include <QZQSM.h>
+#include "QZSSDCX.h"
+
+SFE_UBLOX_GNSS myGNSS;
+
+#ifdef ARDUINO_ESP32C6_DEV
+const int rxPin = 15;
+const int txPin = 21;
+#elif ARDUINO_ESP32S3_DEV
+const int rxPin = 40;
+const int txPin = 35;
+#else  // ESP32_DEV
+const int rxPin = 19;
+const int txPin = 21;
+#endif
+
+#define DBG_PRINT_SFRBX 1
+#define DBG_PRINT_SAT 0
+#define DBG_PRINT_PVT 0
+#define DBG_PRINT_DCX_ALL 0
+
+byte l1s_msg_buf[32];  // MAX 250 BITS
+QZQSM dc_report;
+DCXDecoder dcx_decoder;
+
+// dwrdを16進数文字列に変換して出力する関数
+const char *dwrd_to_str(uint32_t value) {
+  static const char hex_chars[] = "0123456789ABCDEF";  // 16進数文字
+  static char buffer[9];                               // 8桁 + 終端文字
+  // リトルエンディアンなので入れ替える
+  buffer[8] = '\0';
+  buffer[7] = hex_chars[value & 0xF];
+  buffer[6] = hex_chars[value >> 4 & 0xF];
+  buffer[5] = hex_chars[value >> 8 & 0xF];
+  buffer[4] = hex_chars[value >> 12 & 0xF];
+  buffer[3] = hex_chars[value >> 16 & 0xF];
+  buffer[2] = hex_chars[value >> 20 & 0xF];
+  buffer[1] = hex_chars[value >> 24 & 0xF];
+  buffer[0] = hex_chars[value >> 28 & 0xF];
+  return buffer;
+}
+
+void newSFRBX(UBX_RXM_SFRBX_data_t *data) {
+#if DBG_PRINT_SFRBX
+  Serial.print("SFRBX gnssId: ");
+  Serial.print(data->gnssId);
+  Serial.print(" svId: ");
+  Serial.print(data->svId);
+  Serial.print(" freqId: ");
+  Serial.print(data->freqId);
+  Serial.print(" numWords: ");
+  Serial.print(data->numWords);
+  Serial.print(" version: ");
+  Serial.print(data->version);
+  Serial.print(" ");
+  for (int i = 0; i < data->numWords; i++) {
+    Serial.print(dwrd_to_str(data->dwrd[i]));
+  }
+  Serial.println();
+#endif
+
+  // QZSS L1Sメッセージ解析
+  if (data->gnssId == 5) {
+
+    // SFRBXのdwrdはリトルエンディアンなので入れ替える
+    for (int i = 0; i < min(int(data->numWords), 8); i++) {
+      l1s_msg_buf[(i << 2) + 0] = (data->dwrd[i] >> 24) & 0xff;
+      l1s_msg_buf[(i << 2) + 1] = (data->dwrd[i] >> 16) & 0xff;
+      l1s_msg_buf[(i << 2) + 2] = (data->dwrd[i] >> 8) & 0xff;
+      l1s_msg_buf[(i << 2) + 3] = (data->dwrd[i]) & 0xff;
+    }
+
+    byte pab = l1s_msg_buf[0];
+    byte mt = l1s_msg_buf[1] >> 2;
+
+    if (pab == 0x53 || pab == 0x9A || pab == 0xC6) {
+      // Message Typeを表示
+      struct {
+        byte mt;
+        const char *desc;
+      } MTTable[] = {
+        { 0, "Test Mode" },
+        { 43, "DC Report" },
+        { 44, "DCX message" },
+        { 47, "Monitoring Station Information" },
+        { 48, "PRN Mask" },
+        { 49, "Data Issue Number" },
+        { 50, "DGPS Correction" },
+        { 51, "Satellite Health" },
+        { 63, "Null message" },
+      };
+      for (int i = 0; i < sizeof(MTTable) / sizeof(MTTable[0]); i++) {
+        if (MTTable[i].mt == mt) {
+          Serial.print(mt);
+          Serial.print(" ");
+          Serial.println(MTTable[i].desc);
+          break;
+        }
+      }
+      // 災害・危機管理通報サービス（DC Report）のメッセージ内容を表示
+      if (mt == 43) {
+        dc_report.SetYear(2024);
+        dc_report.Decode(l1s_msg_buf);
+        Serial.println(dc_report.GetReport());
+      }
+      // 災害・危機管理通報サービス（拡張）（DCX）のメッセージ内容を表示
+      else if (mt == 44) {
+        dcx_decoder.decode(l1s_msg_buf);
+        dcx_decoder.printSummary(Serial, dcx_decoder.r);
+#if DBG_PRINT_DCX_ALL
+        dcx_decoder.printAll(Serial, dcx_decoder.r);
+#endif
+      }
+    }
+  }
+}
+
+void newNAVSAT(UBX_NAV_SAT_data_t *data) {
+
+  int nGNSS[7] = { 0 };
+  for (uint16_t block = 0; block < data->header.numSvs; block++) {
+    if (data->blocks[block].gnssId < 7) {
+      nGNSS[data->blocks[block].gnssId]++;
+    }
+  }
+  Serial.print(F("Satellites: "));
+  Serial.print(data->header.numSvs);
+  Serial.print(F(" GPS: "));
+  Serial.print(nGNSS[0]);
+  Serial.print(F(" QZSS: "));
+  Serial.print(nGNSS[5]);
+  Serial.print(F(" GLONASS: "));
+  Serial.print(nGNSS[6]);
+  Serial.println();
+
+#if DBG_PRINT_SAT
+  // 衛星の種類を表示
+  for (uint16_t block = 0; block < data->header.numSvs; block++) {
+    switch (data->blocks[block].gnssId) {
+      case 0:
+        Serial.print(F("GPS     "));
+        break;
+      case 1:
+        Serial.print(F("SBAS    "));
+        break;
+      case 2:
+        Serial.print(F("Galileo "));
+        break;
+      case 3:
+        Serial.print(F("BeiDou  "));
+        break;
+      case 4:
+        Serial.print(F("IMES    "));
+        break;
+      case 5:
+        Serial.print(F("QZSS    "));
+        break;
+      case 6:
+        Serial.print(F("GLONASS "));
+        break;
+      default:
+        Serial.print(F("UNKNOWN "));
+        break;
+    }
+
+    // 衛星番号を表示
+    Serial.print(data->blocks[block].svId);
+
+    if (data->blocks[block].svId < 10) Serial.print(F("   "));
+    else if (data->blocks[block].svId < 100) Serial.print(F("  "));
+    else Serial.print(F(" "));
+
+    // 信号の強さを表示
+    for (uint8_t cno = 0; cno < data->blocks[block].cno; cno++)
+      Serial.print(F("|"));
+
+    Serial.println();
+  }
+#endif
+}
+
+void newNAVPVT(UBX_NAV_PVT_data_t *data) {
+#if DBG_PRINT_PVT
+  // 時刻表示
+  Serial.print(F("Time: "));
+  uint8_t hms = data->hour;
+  if (hms < 10) Serial.print(F("0"));
+  Serial.print(hms);
+  Serial.print(F(":"));
+  hms = data->min;
+  if (hms < 10) Serial.print(F("0"));
+  Serial.print(hms);
+  Serial.print(F(":"));
+  hms = data->sec;
+  if (hms < 10) Serial.print(F("0"));
+  Serial.print(hms);
+  Serial.print(F("."));
+  unsigned long millisecs = data->iTOW % 1000;
+  if (millisecs < 100) Serial.print(F("0"));
+  if (millisecs < 10) Serial.print(F("0"));
+  Serial.print(millisecs);
+
+  // 経度・緯度・高度表示
+  long latitude = data->lat;
+  Serial.print(F(" Lat: "));
+  Serial.print(latitude);
+
+  long longitude = data->lon;
+  Serial.print(F(" Long: "));
+  Serial.print(longitude);
+  Serial.print(F(" (degrees * 10^-7)"));
+
+  long altitude = data->hMSL;
+  Serial.print(F(" Height above MSL: "));
+  Serial.print(altitude);
+  Serial.print(F(" (mm)"));
+
+  Serial.print(F(" SIV: "));
+  Serial.print(data->numSV);
+
+  Serial.print(F(" Fix: "));
+  Serial.print(data->fixType);
+
+  Serial.println();
+#endif
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    ;  //ターミナルを開くまでまつ
+
+  Serial1.begin(9600, SERIAL_8N1, rxPin, txPin);
+
+  // myGNSS.enableDebugging(); // デバッグメッセージをSerialに出力する
+  if (myGNSS.begin(Serial1) == false)  // Serial1を介してu-bloxモジュールに接続する
+  {
+    // ボーレートを変えてトライ
+    Serial1.begin(38400, SERIAL_8N1, rxPin, txPin);
+    if (myGNSS.begin(Serial1) == false) {
+      Serial.println(F("u-blox GNSS not detected. Please check wiring. Freezing."));
+      while (1)
+        ;
+    }
+  }
+
+  myGNSS.setUART1Output(COM_TYPE_UBX);           // UART1から出力をUBXのみに設定する
+  enableQZSSL1S();                               // QZSS L1S信号の受信を有効にする
+  myGNSS.setAutoRXMSFRBXcallbackPtr(&newSFRBX);  // UBX-RXM-SFRBXメッセージ受信コールバック関数を登録
+  myGNSS.setAutoNAVSATcallbackPtr(&newNAVSAT);   // UBX-NAV-SATメッセージ受信コールバック関数を登録
+  myGNSS.setAutoPVTcallbackPtr(&newNAVPVT);      // UBX-NAV-PVTメッセージ受信コールバック関数を登録
+}
+
+// QZSSのL1S信号を受信するよう設定する
+bool enableQZSSL1S(void) {
+  uint8_t customPayload[MAX_PAYLOAD_SIZE];
+  ubxPacket customCfg = { 0, 0, 0, 0, 0, customPayload, 0, 0, SFE_UBLOX_PACKET_VALIDITY_NOT_DEFINED, SFE_UBLOX_PACKET_VALIDITY_NOT_DEFINED };
+
+  customCfg.cls = UBX_CLASS_CFG;
+  customCfg.id = UBX_CFG_GNSS;
+  customCfg.len = 0;
+  customCfg.startingSpot = 0;
+
+  if (myGNSS.sendCommand(&customCfg) != SFE_UBLOX_STATUS_DATA_RECEIVED)
+    return (false);
+
+  int numConfigBlocks = customPayload[3];
+  for (int block = 0; block < numConfigBlocks; block++) {
+    if (customPayload[(block * 8) + 4] == (uint8_t)SFE_UBLOX_GNSS_ID_QZSS) {
+      customPayload[(block * 8) + 8] |= 0x01;      // set enable bit
+      customPayload[(block * 8) + 8 + 2] |= 0x05;  // set 0x01 QZSS L1C/A 0x04 = QZSS L1S
+    }
+  }
+
+  return (myGNSS.sendCommand(&customCfg) == SFE_UBLOX_STATUS_DATA_SENT);
+}
+
+void loop() {
+  myGNSS.checkUblox();
+  myGNSS.checkCallbacks();
+}
